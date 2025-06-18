9a226937544ed6407d43c605cf38522e8d469fae
AxelSoler <axelss369@gmail.com>
2025-06-18 12:53:06 -0300

    Few changes to about page

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,139 +1,167 @@
-import HTMLimg from '../../img/symbols/html.svg';
-import CSSimg from '../../img/symbols/css.svg';
-import JavaScriptimg from '../../img/symbols/javascript.svg';
-import reactimg from '../../img/symbols/react.svg';
-import Rubyimg from '../../img/symbols/ruby.svg';
-import Railsimg from '../../img/symbols/rails.svg';
-import Postgresimg from '../../img/symbols/postgresql.svg';
-import RSpecimg from '../../img/symbols/RSpec.svg';
-import Githubimg from '../../img/symbols/github.svg';
-import Gitlabimg from '../../img/symbols/gitlab.svg';
-import Terminalimg from '../../img/symbols/terminal.svg';
+import HTMLimg from "../../img/symbols/html.svg";
+import CSSimg from "../../img/symbols/css.svg";
+import JavaScriptimg from "../../img/symbols/javascript.svg";
+import reactimg from "../../img/symbols/react.svg";
+import Rubyimg from "../../img/symbols/ruby.svg";
+import Railsimg from "../../img/symbols/rails.svg";
+import Postgresimg from "../../img/symbols/postgresql.svg";
+import RSpecimg from "../../img/symbols/RSpec.svg";
+import Githubimg from "../../img/symbols/github.svg";
+import Gitlabimg from "../../img/symbols/gitlab.svg";
 import profilePicture from "@/img/profile/profilepic.jpg";
-import Image from 'next/image';
-import './About.css';
+import Image from "next/image";
+import "./About.css";
 
 const About = () => (
-  <section className="about">
-    <h1 className="myName">AXEL SOLER</h1>
-    <h2 className="Developer">Full Stack Developer</h2>
-    <div className="aboutDescription">
-      <div className="marquee">
-        <div className="marqueeblock">
-          <ul className="ulmarquee">
-            <li>
-              <Image className="skill" src={HTMLimg} alt="skill logo" />
-              HTML
-            </li>
-            <li>
-              <Image className="skill" src={CSSimg} alt="skill logo" />
-              CSS
-            </li>
-            <li>
-              <Image className="skill" src={JavaScriptimg} alt="skill logo" />
-              JavaScript
-            </li>
-            <li>
-              <Image className="skill" src={reactimg} alt="skill logo" />
-              React
-            </li>
-            <li>
-              <Image className="skill" src={Rubyimg} alt="skill logo" />
-              Ruby
-            </li>
-            <li>
-              <Image className="skill railsimg" src={Railsimg} alt="skill logo" />
-              Rails
-            </li>
-            <li>
-              <Image className="skill" src={Postgresimg} alt="skill logo" />
-              Postgres
-            </li>
-            <li>
-              <Image className="skill" src={RSpecimg} alt="skill logo" />
-              RSpec
-            </li>
-            <li>
-              <Image className="skill" src={Githubimg} alt="skill logo" />
-              Github
-            </li>
-            <li>
-              <Image className="skill" src={Gitlabimg} alt="skill logo" />
-              Gitlab
-            </li>
-            <li>
-              <Image className="skill" src={Terminalimg} alt="skill logo" />
-              Terminal
-            </li>
-          </ul>
+  <section
+    className="text-center flex flex-col items-center text-white"
+    id="about"
+  >
+    <h1 className="text-3xl">AXEL SOLER</h1>
+    <h2 className="text-2xl">Full Stack Developer</h2>
+    <div className="flex flex-col items-center lg:w-1/2">
+      <Image
+        className="rounded-full w-60 border-4 border-[#4294ff]"
+        src={profilePicture}
+        alt="Axel Soler"
+      />
+      <div className="h-28 w-full mt-4">
+        <div className="marquee">
+          <div className="marqueeblock">
+            <ul className="ulmarquee">
+              <li>
+                <Image className="skill" src={HTMLimg} alt="skill logo" />
+                HTML
+              </li>
+              <li>
+                <Image className="skill" src={CSSimg} alt="skill logo" />
+                CSS
+              </li>
+              <li>
+                <Image className="skill" src={JavaScriptimg} alt="skill logo" />
+                JavaScript
+              </li>
+              <li>
+                <Image className="skill" src={reactimg} alt="skill logo" />
+                React
+              </li>
+              <li>
+                <Image className="skill" src={Rubyimg} alt="skill logo" />
+                Ruby
+              </li>
+              <li>
+                <Image
+                  className="skill railsimg"
+                  src={Railsimg}
+                  alt="skill logo"
+                />
+                Rails
+              </li>
+              <li>
+                <Image className="skill" src={Postgresimg} alt="skill logo" />
+                Postgres
+              </li>
+              <li>
+                <Image className="skill" src={RSpecimg} alt="skill logo" />
+                RSpec
+              </li>
+              <li>
+                <Image className="skill" src={Githubimg} alt="skill logo" />
+                Github
+              </li>
+              <li>
+                <Image className="skill" src={Gitlabimg} alt="skill logo" />
+                Gitlab
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div className="marquee">
+          <div className="marqueeblock marqueeblock2">
+            <ul className="ulmarquee">
+              <li>
+                <Image className="skill" src={HTMLimg} alt="skill logo" />
+                HTML
+              </li>
+              <li>
+                <Image className="skill" src={CSSimg} alt="skill logo" />
+                CSS
+              </li>
+              <li>
+                <Image className="skill" src={JavaScriptimg} alt="skill logo" />
+                JavaScript
+              </li>
+              <li>
+                <Image className="skill" src={reactimg} alt="skill logo" />
+                React
+              </li>
+              <li>
+                <Image className="skill" src={Rubyimg} alt="skill logo" />
+                Ruby
+              </li>
+              <li>
+                <Image
+                  className="skill railsimg"
+                  src={Railsimg}
+                  alt="skill logo"
+                />
+                Rails
+              </li>
+              <li>
+                <Image className="skill" src={Postgresimg} alt="skill logo" />
+                Postgres
+              </li>
+              <li>
+                <Image className="skill" src={RSpecimg} alt="skill logo" />
+                RSpec
+              </li>
+              <li>
+                <Image className="skill" src={Githubimg} alt="skill logo" />
+                Github
+              </li>
+              <li>
+                <Image className="skill" src={Gitlabimg} alt="skill logo" />
+                Gitlab
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
-      <div className="marquee">
-        <div className="marqueeblock marqueeblock2">
-          <ul className="ulmarquee">
-            <li>
-              <Image className="skill" src={HTMLimg} alt="skill logo" />
-              HTML
-            </li>
-            <li>
-              <Image className="skill" src={CSSimg} alt="skill logo" />
-              CSS
-            </li>
-            <li>
-              <Image className="skill" src={JavaScriptimg} alt="skill logo" />
-              JavaScript
-            </li>
-            <li>
-              <Image className="skill" src={reactimg} alt="skill logo" />
-              React
-            </li>
-            <li>
-              <Image className="skill" src={Rubyimg} alt="skill logo" />
-              Ruby
-            </li>
-            <li>
-              <Image className="skill railsimg" src={Railsimg} alt="skill logo" />
-              Rails
-            </li>
-            <li>
-              <Image className="skill" src={Postgresimg} alt="skill logo" />
-              Postgres
-            </li>
-            <li>
-              <Image className="skill" src={RSpecimg} alt="skill logo" />
-              RSpec
-            </li>
-            <li>
-              <Image className="skill" src={Githubimg} alt="skill logo" />
-              Github
-            </li>
-            <li>
-              <Image className="skill" src={Gitlabimg} alt="skill logo" />
-              Gitlab
-            </li>
-            <li>
-              <Image className="skill" src={Terminalimg} alt="skill logo" />
-              Terminal
-            </li>
-          </ul>
-        </div>
-      </div>
-      <Image className="profilePicture" src={profilePicture} alt="Axel Soler" id="about" />
-      <p className="description">I am passionate about programming and video games. Learning and working with teammates and friends around the world in a fully remote environment.</p>
-      <p className="SkillsDescription">
+      <p className="description">
+        I’m a Full Stack Developer with a strong focus on Frontend technologies.
+        I specialize in building modern, responsive, and high-performance web
+        applications using React, Next.js, and Tailwind CSS.
+      </p>
+
+      <p className="description">
+        I’m passionate about programming, and I thrive in
+        collaborative, fully remote environments where I can learn and grow
+        alongside teammates from around the world.
+      </p>
+
+      {/* <p className="SkillsDescription">
         <Image className="skill" src={JavaScriptimg} alt="JavaScript logo" />
-        JavaScript =&gt; React
+        JavaScript → React
         <Image className="skill" src={reactimg} alt="React logo" />
       </p>
       <p className="SkillsDescription">
-        <Image className="skill" src={Rubyimg} alt="skill logo" />
-        Ruby =&gt; Rails
-        <Image className="skill railsimg" src={Railsimg} alt="skill logo" />
+        <Image className="skill" src={Rubyimg} alt="Ruby logo" />
+        Ruby → Ruby on Rails
+        <Image className="skill railsimg" src={Railsimg} alt="Rails logo" />
+      </p> */}
+
+      <p className="description">
+        I’m patient, collaborative, committed, and always looking to improve. I
+        bring a growth mindset and a friendly attitude to every project.
       </p>
-      <p className="description">Patient, Teamwork, Compromise, growth mindset, and friendly.</p>
-      <p className="description">English and Spanish (Argentina).</p>
-      <p className="description">Look through some of my experience in the Projects section!</p>
-      <p className="description">If you like what you see, don’t hesitate to contact me in the Contact section.</p>
+      <p className="description">
+        I’m fluent in both English and Spanish.
+      </p>
+      <p className="description">
+        Feel free to explore my work in the Projects section. If something
+        catches your eye, don’t hesitate to reach out through the Contact
+        section.
+      </p>
     </div>
   </section>
 );
