0c044a3972e786fe948369d85d7c681f245ee246
AxelSoler <axelss369@gmail.com>
2025-06-22 15:16:33 -0300

    At more info to projects and private link option

--- src/components/projects/Project.tsx
+++ src/components/projects/Project.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import type { StaticImageData } from 'next/image';
+import type { StaticImageData } from "next/image";
 import { FaGithub } from "react-icons/fa";
 import { HiOutlineExternalLink } from "react-icons/hi";
 
@@ -10,39 +10,63 @@ type projectProps = {
   live: string;
   repository: string;
   technologies: string[];
+  position: string;
+  company: string;
 };
 
 const Project = (props: { project: projectProps }) => {
   const { project } = props;
   return (
-    <div id={project.name} className="project bg-gray-600/30 dark:bg-black/60">
+    <div
+      id={project.name}
+      className="project text-md md:text-lg gap-2 bg-gray-600/30 dark:bg-black/60"
+    >
       <Image className="picture" src={project.image} alt={project.name} />
-      <h3 className="text-xl md:text-3xl">{project.name}</h3>
-      <p className="text-lg md:text-xl">{project.description}</p>
-      <ul className="flex flex-wrap gap-2 justify-center text-lg md:text-xl">
-        {project.technologies.map((technology: string) => (
-          <li key={technology}>{technology}</li>
-        ))}
-      </ul>
-      <div className="flex justify-between items-center w-full">
-        <a
-          href={project.live}
-          target="_blank"
-          rel="noopener noreferrer"
-          className="projectLink"
-        >
-          Live Site
-          <HiOutlineExternalLink />
-        </a>
-        <a
-          href={project.repository}
-          target="_blank"
-          rel="noopener noreferrer"
-          className="projectLink"
-        >
-          View Code
-          <FaGithub />
-        </a>
+      <h3 className="text-xl md:text-3xl mb-2">{project.name}</h3>
+      <p className="md:text-xl">{project.description}</p>
+      <p className="mr-auto">Company: {project.company}</p>
+      <p className="mr-auto">Role: {project.position}</p>
+      <p className="flex gap-2 mr-auto">
+        Stack:
+        <ul className="flex flex-wrap gap-2 justify-start">
+          {project.technologies.map((technology: string) => (
+            <li key={technology}>{technology}</li>
+          ))}
+        </ul>
+      </p>
+      <div className="flex justify-between items-center w-full mt-auto">
+        {project.live === "private" ? (
+          <span className="projectLink cursor-not-allowed opacity-50 flex items-center gap-1">
+            Private
+            <HiOutlineExternalLink />
+          </span>
+        ) : (
+          <a
+            href={project.live}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="projectLink cursor-pointer flex items-center gap-1"
+          >
+            Live Site
+            <HiOutlineExternalLink />
+          </a>
+        )}
+        {project.repository === "private" ? (
+          <span className="projectLink cursor-not-allowed opacity-50 flex items-center gap-1">
+            Private
+            <FaGithub />
+          </span>
+        ) : (
+          <a
+            href={project.repository}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="projectLink cursor-pointer flex items-center gap-1"
+          >
+            Live Site
+            <FaGithub />
+          </a>
+        )}
       </div>
     </div>
   );
--- src/components/projects/ProjectsList.js
+++ src/components/projects/ProjectsList.js
@@ -1,101 +1,101 @@
-import yellowroomsimg from '../../img/projects/YellowRooms.png';
-import monsterimg from '../../img/projects/MonsterHunterWorld.png';
-import budgetimg from '../../img/projects/SuperBudgetApp.jpg';
-import spaceimg from '../../img/projects/SpaceTravelersHub.png';
-import cocktailimg from '../../img/projects/Cocktail.png';
-import sonicimg from '../../img/projects/SonicLeaderboard.png';
-import mycroverseimg from '../../img/projects/Mycroverse2.0.png';
+import yellowroomsimg from "../../img/projects/YellowRooms.png";
+import monsterimg from "../../img/projects/MonsterHunterWorld.png";
+import budgetimg from "../../img/projects/SuperBudgetApp.jpg";
+import spaceimg from "../../img/projects/SpaceTravelersHub.png";
+import cocktailimg from "../../img/projects/Cocktail.png";
+import sonicimg from "../../img/projects/SonicLeaderboard.png";
+import mycroverseimg from "../../img/projects/Mycroverse2.0.png";
 
 const projects = [
   {
-    name: 'YellowRooms',
-    description: 'A full-stack hotel booking platform where users can search, reserve, and manage their stay in stylish Yellow Rooms. Built with React and Ruby on Rails for a seamless experience.',
+    name: "American Fleet Insurance",
+    description:
+      "American Fleet Insurance is the fastest growing commercial vehicle insurance agency in the US. We build operational software to decrease the sales process timeline.",
     image: yellowroomsimg,
-    technologies: [
-      'JavaScript',
-      'React',
-      'CSS',
-      'Ruby',
-      'Rails',
-    ],
-    live: 'https://yellow-rooms.netlify.app/',
-    repository: 'https://github.com/AxelSoler/YellowRooms-Front',
+    technologies: ["JavaScript", "React", "CSS", "Tailwind"],
+    live: "private",
+    repository: "private",
+    position: "Front End Developer",
+    company: "GoodSoftwareDev"
   },
   {
-    name: 'Monster Hunter World',
-    description: 'An interactive web application that provides detailed data and statistics on all monsters from the Monster Hunter World game. Users can filter, explore, and learn about each creature’s behavior and attributes.',
+    name: "YellowRooms",
+    description:
+      "A full-stack hotel booking platform where users can search, reserve, and manage their stay in stylish Yellow Rooms. Built with React and Ruby on Rails for a seamless experience.",
+    image: yellowroomsimg,
+    technologies: ["JavaScript", "React", "CSS", "Ruby", "Rails"],
+    live: "https://yellow-rooms.netlify.app/",
+    repository: "https://github.com/AxelSoler/YellowRooms-Front",
+    position: "Full Stack Developer",
+    company: "Microverse"
+  },
+  {
+    name: "Monster Hunter World",
+    description:
+      "An interactive web application that provides detailed data and statistics on all monsters from the Monster Hunter World game. Users can filter, explore, and learn about each creature’s behavior and attributes.",
     image: monsterimg,
-    technologies: [
-      'CSS',
-      'JavaScript',
-      'React',
-      'Redux',
-    ],
-    live: 'https://classy-banoffee-260a19.netlify.app/',
-    repository: 'https://github.com/AxelSoler/monster-hunter-world',
+    technologies: ["CSS", "JavaScript", "React", "Redux"],
+    live: "https://classy-banoffee-260a19.netlify.app/",
+    repository: "https://github.com/AxelSoler/monster-hunter-world",
+    position: "Full Stack Developer",
+    company: "Microverse"
   },
   {
-    name: 'Super Budget App',
-    description: 'A mobile-first budgeting app that helps users track their expenses and categorize transactions. Built with Ruby on Rails and tested with RSpec to ensure performance and reliability.',
+    name: "Super Budget App",
+    description:
+      "A mobile-first budgeting app that helps users track their expenses and categorize transactions. Built with Ruby on Rails and tested with RSpec to ensure performance and reliability.",
     image: budgetimg,
-    technologies: [
-      'CSS',
-      'Ruby',
-      'Rails',
-      'RSpec',
-    ],
-    live: 'https://super-budget-app.fly.dev/',
-    repository: 'https://github.com/AxelSoler/super-budget-app',
+    technologies: ["CSS", "Ruby", "Rails", "RSpec"],
+    live: "private",
+    repository: "https://github.com/AxelSoler/super-budget-app",
+    position: "Full Stack Developer",
+    company: "Microverse"
   },
   {
-    name: 'Space Travelers Hub',
-    description: 'A space-themed web app where users can book missions and join rockets. It simulates a commercial space travel experience, integrating Redux for efficient state management.',
+    name: "Space Travelers Hub",
+    description:
+      "A space-themed web app where users can book missions and join rockets. It simulates a commercial space travel experience, integrating Redux for efficient state management.",
     image: spaceimg,
-    technologies: [
-      'CSS',
-      'JavaScript',
-      'React',
-      'Redux',
-    ],
-    live: 'https://spiffy-marigold-45bbb6.netlify.app/',
-    repository: 'https://github.com/AxelSoler/space-travelers-hub',
+    technologies: ["CSS", "JavaScript", "React", "Redux"],
+    live: "https://spiffy-marigold-45bbb6.netlify.app/",
+    repository: "https://github.com/AxelSoler/space-travelers-hub",
+    position: "Front End Developer",
+    company: "Microverse"
   },
   {
-    name: 'Cocktail',
-    description: 'A dynamic interface to explore Margarita cocktail recipes using an external API. Users can view detailed descriptions, leave likes, and add comments to their favorite drinks.',
+    name: "Cocktail",
+    description:
+      "A dynamic interface to explore Margarita cocktail recipes using an external API. Users can view detailed descriptions, leave likes, and add comments to their favorite drinks.",
     image: cocktailimg,
-    technologies: [
-      'CSS',
-      'HTML',
-      'JavaScript',
-    ],
-    live: 'https://axelsoler.github.io/JavaScript-capstone-project/dist/',
-    repository: 'https://github.com/AxelSoler/JavaScript-capstone-project',
+    technologies: ["CSS", "HTML", "JavaScript"],
+    live: "https://axelsoler.github.io/JavaScript-capstone-project/dist/",
+    repository: "https://github.com/AxelSoler/JavaScript-capstone-project",
+    position: "Front End Developer",
+    company: "Microverse"
   },
   {
-    name: 'Sonic Leaderboard',
-    description: 'A real-time leaderboard application inspired by Sonic, where players can submit and view scores. Fully responsive and powered by asynchronous JavaScript and an external API.',
+    name: "Sonic Leaderboard",
+    description:
+      "A real-time leaderboard application inspired by Sonic, where players can submit and view scores. Fully responsive and powered by asynchronous JavaScript and an external API.",
     image: sonicimg,
-    technologies: [
-      'CSS',
-      'HTML',
-      'JavaScript',
-    ],
-    live: 'https://axelsoler.github.io/Leaderboard/dist/',
-    repository: 'https://github.com/AxelSoler/Leaderboard',
+    technologies: ["CSS", "HTML", "JavaScript"],
+    live: "https://axelsoler.github.io/Leaderboard/dist/",
+    repository: "https://github.com/AxelSoler/Leaderboard",
+    position: "Front End Developer",
+    company: "Microverse"
   },
   {
-    name: 'Mycroverse-2.0',
-    description: 'A sleek and modern landing page promoting an educational program. Highlights the course benefits and encourages visitors to enroll through interactive design.',
+    name: "Mycroverse-2.0",
+    description:
+      "A sleek and modern landing page promoting an educational program. Highlights the course benefits and encourages visitors to enroll through interactive design.",
     image: mycroverseimg,
-    technologies: [
-      'CSS',
-      'HTML',
-      'JavaScript',
-    ],
-    live: 'https://axelsoler.github.io/HTML-CSS-JavaScript-capstone-project---Mycroverse-2.0/',
-    repository: 'https://github.com/AxelSoler/HTML-CSS-JavaScript-capstone-project---Mycroverse-2.0',
-  },
+    technologies: ["CSS", "HTML", "JavaScript"],
+    live: "https://axelsoler.github.io/HTML-CSS-JavaScript-capstone-project---Mycroverse-2.0/",
+    repository:
+      "https://github.com/AxelSoler/HTML-CSS-JavaScript-capstone-project---Mycroverse-2.0",
+    position: "Front End Developer",
+    company: "Microverse"
+  }
 ];
 
 export default projects;
