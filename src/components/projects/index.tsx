cd88d362d6e26e8f851314055e7b01ef88457551
AxelSoler <axelss369@gmail.com>
2025-06-23 18:20:54 -0300

    Remove carousel and projects page

--- src/components/projects/index.tsx
+++ src/components/projects/index.tsx
@@ -1,61 +1,51 @@
 "use client";
-import Carousel from "react-multi-carousel";
-import "react-multi-carousel/lib/styles.css";
+import { useState } from "react";
 import projects from "./ProjectsList";
 import Project from "./Project";
 import "./index.css";
-import Link from "next/link";
+import { FiChevronDown } from "react-icons/fi";
 
 const Projects = () => {
-  const responsive = {
-    desktop: {
-      breakpoint: { max: 3000, min: 1392 },
-      items: 3
-    },
-    laptop: {
-      breakpoint: { max: 1392, min: 768 },
-      items: 2
-    },
-    mobile: {
-      breakpoint: { max: 768, min: 0 },
-      items: 1
-    }
+  const [visibleCount, setVisibleCount] = useState(2);
+
+  const handleLoadMore = () => {
+    setVisibleCount((prev) => prev + 2);
   };
   return (
-    <section className="pt-6 w-full md:w-4/5 mx-auto " id="projects">
+    <section
+      className="pt-6 w-full md:w-4/5 mx-auto space-y-2 md:space-y-4"
+      id="projects"
+    >
       <h2 className="text-2xl md:text-3xl lg:text-4xl font-bold underline md:mt-16 mb-10 text-center">
-        Latest Creations
+        Experience
       </h2>
-      <Carousel
-        arrows={true}
-        responsive={responsive}
-        className="carousel-container"
-        infinite
-        centerMode={false}
-        additionalTransfrom={0}
-        draggable={false}
-        focusOnSelect={false}
-        keyBoardControl
-        minimumTouchDrag={80}
-        renderArrowsWhenDisabled={false}
-        rewind={false}
-        rewindWithAnimation={false}
-        rtl={false}
-        slidesToSlide={1}
-        swipeable
-        transitionDuration={500}
-      >
-        {projects.slice(0, 5).map((project) => (
-          <Project key={project.name} project={project} />
-        ))}
-      </Carousel>
-      <div className="w-full flex justify-center">
-        <Link
-          href="/projects"
-          className="inline-block mt-8 px-6 py-3 bg-gradient-to-r from-blue-500 to-indigo-600 text-white font-medium rounded-full shadow-lg hover:scale-105 transform transition duration-300"
+      {projects.slice(0, visibleCount).map((project, index) => {
+        const isEven = index % 2 === 0;
+
+        return (
+          <Project
+            key={project.name}
+            project={project}
+            isEven={isEven}
+          ></Project>
+        );
+      })}
+
+      <div className="flex justify-center w-full">
+        <button
+          onClick={handleLoadMore}
+          className={`mt-4 px-6 w-full md:w-auto py-4 bg-gradient-to-r from-blue-500 to-indigo-600 text-white font-medium md:text-lg rounded-full shadow-lg transform transition duration-300 
+          ${
+            visibleCount >= projects.length
+              ? "opacity-50 cursor-not-allowed"
+              : "hover:scale-105 cursor-pointer"
+          }`}
+          disabled={visibleCount >= projects.length}
         >
-          See All Projects
-        </Link>
+          <FiChevronDown className="inline mr-2 text-2xl" />
+          Load More Projects
+          <FiChevronDown className="inline ml-2 text-2xl" />
+        </button>
       </div>
     </section>
   );
