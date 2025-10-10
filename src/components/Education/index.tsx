e5707723cafd920dd67ae887a78c8776eacc0bb3
AxelSoler <axelss369@gmail.com>
2025-10-10 12:47:34 -0300

    Education translate

--- src/components/Education/index.tsx
+++ src/components/Education/index.tsx
@@ -1,50 +1,30 @@
-"use client";
+import { useTranslations } from "next-intl";
 
-const Education = () => (
-  <section
-    className="pt-6 w-full md:w-4/5 mx-auto space-y-2 md:space-y-4"
-    id="projects"
-  >
-    <div className="p-4 md:p-6 bg-gray-600/30 dark:bg-black/60 rounded-2xl">
-      <h2 className="text-2xl md:text-3xl lg:text-4xl font-bold underline mb-4 text-center">
-        Education
-      </h2>
-      <div>
-        <h3 className="text-xl md:text-2xl font-semibold">
-          Full Stack Developer – Microverse
-        </h3>
-        <p className="text-sm italic">Mar 2022 – Dec 2022</p>
-        <p className="mt-2 text-base leading-relaxed md:text-lg">
-          In 2022, I joined Microverse, an intensive, full-time remote software
-          development program where I dedicated over 1300 hours to mastering
-          full-stack web development. The curriculum focused heavily on
-          JavaScript, React, Redux, and Ruby on Rails, with a strong emphasis on
-          algorithms, data structures, and building scalable applications
-          through project-based learning.
-          <br />
-          <br />
-          Through daily remote pair programming sessions and weekly team
-          projects, I gained hands-on experience collaborating with developers
-          from all over the world, simulating real-world agile environments. I
-          practiced industry-standard Git workflows, conducted code reviews, and
-          participated in daily stand-ups to ensure transparent, async
-          communication.
-          <br />
-          <br />
-          Beyond technical development, I also mentored junior developers,
-          supporting them in understanding React best practices, testing
-          methodologies, and debugging strategies. This experience deepened my
-          leadership and communication skills, while reinforcing my commitment
-          to writing clean, maintainable, and scalable code.
-          <br />
-          <br />
-          Microverse prepared me not only as a developer but as a remote
-          professional—ready to thrive in globally distributed teams and
-          continuously adapt to new challenges in fast-paced environments.
-        </p>
+const Education = () => {
+  const t = useTranslations("Education");
+  return (
+    <section
+      className="pt-6 w-full md:w-4/5 mx-auto space-y-2 md:space-y-4"
+      id="projects"
+    >
+      <div className="p-4 md:p-6 bg-gray-600/30 dark:bg-black/60 rounded-2xl">
+        <h2 className="text-2xl md:text-3xl lg:text-4xl font-bold underline mb-4 text-center">
+          {t("title")}
+        </h2>
+        <div>
+          <h3 className="text-xl md:text-2xl font-semibold">
+            {t("degree")} – Microverse
+          </h3>
+          <p className="text-sm italic">{t("date")}</p>
+          <p className="mt-2 text-base leading-relaxed md:text-lg">
+            {t.rich("microverse", {
+              br: () => <br />,
+            })}
+          </p>
+        </div>
       </div>
-    </div>
-  </section>
-);
+    </section>
+  );
+};
 
 export default Education;
