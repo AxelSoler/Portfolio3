064de534f23743a8465c093685b6dae103558a3a
AxelSoler <axelss369@gmail.com>
2025-10-10 15:53:13 -0300

    Add li to ul

--- src/components/about/index.tsx
+++ src/components/about/index.tsx
@@ -1,7 +1,7 @@
 import Image from "next/image";
 import Link from "next/link";
 // import profilePicture from "@/img/profile/profilepic.jpg";
-import logo from "@/img/profile/logo.png";
+import logo from "@/img/profile/light_logo.png";
 import { RiNextjsFill, RiTailwindCssFill, RiNodejsLine } from "react-icons/ri";
 import {
   SiRubyonrails,
--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -2,7 +2,8 @@
 
 import Image from "next/image";
 import { IoIosArrowForward } from "react-icons/io";
-import logo from "@/img/profile/logo.png";
+import lightLogo from "@/img/profile/light_logo.png";
+import darkLogo from "@/img/profile/dark_logo.png";
 import "./index.css";
 import { useState } from "react";
 import ThemeToggle from "../ThemeToggle";
@@ -27,40 +28,52 @@ const Navbar = () => {
           isOpen ? "flex" : "hidden"
         } bg-gradient-to-t from-slate-400 via-slate-100 to-slate-400 shadow-md dark:bg-gradient-to-t dark:from-[#0a0a0a] dark:via-[#004aad] dark:to-[#0a0a0a] transition-colors duration-500 ease-in-out`}
       >
-        <Image
-          className="navProfilePicture"
-          src={logo}
-          alt="Axel Soler"
-        />
-        <a
-          className="pageLink border border-black dark:border-white rounded-xl bg-gradient-to-l from-slate-400 via-slate-100 shadow-md dark:bg-gradient-to-l dark:from-[#0a0a0a] dark:via-[#004aad] transition-colors duration-500 ease-in-out"
-          id="aboutLink"
-          href="#about"
-          onClick={toggleMobileMenu}
-        >
-          {t("about")}
-          <IoIosArrowForward size="28px" />
-        </a>
-        <a
-          className="pageLink border border-black dark:border-white rounded-xl bg-gradient-to-l from-slate-400 via-slate-100 shadow-md dark:bg-gradient-to-l dark:from-[#0a0a0a] dark:via-[#004aad] transition-colors duration-500 ease-in-out"
-          id="projectsLink"
-          href="#projects"
-          onClick={toggleMobileMenu}
-        >
-          {t("projects")}
-          <IoIosArrowForward size="28px" />
-        </a>
-        <a
-          className="pageLink border border-black dark:border-white rounded-xl bg-gradient-to-l from-slate-400 via-slate-100 shadow-md dark:bg-gradient-to-l dark:from-[#0a0a0a] dark:via-[#004aad] transition-colors duration-500 ease-in-out"
-          id="contactLink"
-          href="#contact"
-          onClick={toggleMobileMenu}
-        >
-          {t("contact")}
-          <IoIosArrowForward size="28px" />
-        </a>
-        <ThemeToggle />
-        <LanguageToggle />
+        <li className="w-full">
+          <Image
+            className="navProfilePicture"
+            src={lightLogo}
+            alt="Axel Soler"
+          />
+        </li>
+        <li className="w-full">
+          <a
+            className="pageLink border border-black dark:border-white rounded-xl bg-gradient-to-l from-slate-400 via-slate-100 shadow-md dark:bg-gradient-to-l dark:from-[#0a0a0a] dark:via-[#004aad] transition-colors duration-500 ease-in-out"
+            id="aboutLink"
+            href="#about"
+            onClick={toggleMobileMenu}
+          >
+            {t("about")}
+            <IoIosArrowForward size="28px" />
+          </a>
+        </li>
+        <li className="w-full">
+          <a
+            className="pageLink border border-black dark:border-white rounded-xl bg-gradient-to-l from-slate-400 via-slate-100 shadow-md dark:bg-gradient-to-l dark:from-[#0a0a0a] dark:via-[#004aad] transition-colors duration-500 ease-in-out"
+            id="projectsLink"
+            href="#projects"
+            onClick={toggleMobileMenu}
+          >
+            {t("projects")}
+            <IoIosArrowForward size="28px" />
+          </a>
+        </li>
+        <li className="w-full">
+          <a
+            className="pageLink border border-black dark:border-white rounded-xl bg-gradient-to-l from-slate-400 via-slate-100 shadow-md dark:bg-gradient-to-l dark:from-[#0a0a0a] dark:via-[#004aad] transition-colors duration-500 ease-in-out"
+            id="contactLink"
+            href="#contact"
+            onClick={toggleMobileMenu}
+          >
+            {t("contact")}
+            <IoIosArrowForward size="28px" />
+          </a>
+        </li>
+        <li className="w-full">
+          <ThemeToggle />
+        </li>
+        <li className="w-full">
+          <LanguageToggle />
+        </li>
       </ul>
       <button
         type="button"
