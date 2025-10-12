cfaa1acaf4aca9933a0b50755c187f7c445b4423
AxelSoler <axelss369@gmail.com>
2025-10-12 11:27:01 -0300

    More spacing

--- src/components/contact/index.tsx
+++ src/components/contact/index.tsx
@@ -91,7 +91,7 @@ const ContactForm = () => {
           <h2 className="text-2xl md:text-3xl lg:text-5xl font-bold underline">
             {t("title")}
           </h2>
-          <p className="text-lg md:text-2xl font-bold my-2">
+          <p className="text-lg md:text-2xl font-bold mt-2 md:mt-4 lg:mt-8">
             {t("subtitle")}
           </p>
           <label className="labelInput" htmlFor="username">
