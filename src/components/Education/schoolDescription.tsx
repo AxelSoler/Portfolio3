3806408fa95a63e2fca556be9b69a5aa600fd54c
AxelSoler <axelss369@gmail.com>
2025-10-14 10:59:46 -0300

    More style to education card

--- src/components/Education/schoolDescription.tsx
+++ src/components/Education/schoolDescription.tsx
@@ -16,7 +16,7 @@ export default function Schooldescription({
   const [expanded, setExpanded] = useState(false);
 
   return (
-    <div className="mt-2 text-base leading-relaxed md:text-lg">
+    <div className="my-8 lg:my-12 text-base leading-relaxed md:text-lg bg-gradient-to-b dark:from-[#0a0f1f] dark:to-[#0e1630] p-4 rounded-2xl shadow-lg border border-violet-600 dark:border-white/10 transition-transform hover:scale-[1.02] hover:shadow-xl duration-300">
       <div className="flex justify-between gap-4 items-start">
         <div>
           <h3 className="text-xl md:text-2xl font-semibold">{t("degree")}</h3>
