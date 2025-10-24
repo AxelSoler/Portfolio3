d6df201c4fd40787da0b4cb91c0225a6f433fd6c
AxelSoler <axelss369@gmail.com>
2025-10-24 10:56:48 -0300

    Add metadata to main layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,26 +7,68 @@ import { NextIntlClientProvider } from "next-intl";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
-  subsets: ["latin"]
+  subsets: ["latin"],
 });
 
 const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
-  subsets: ["latin"]
+  subsets: ["latin"],
 });
 
 export const metadata: Metadata = {
   title: "Axel Soler Portfolio",
-  description: "Axel Soler Portfolio"
+  description: "Axel Soler Portfolio",
+  keywords: [
+    "Software Developer",
+    "JavaScript",
+    "Full-Stack",
+    "React",
+    "Node.js",
+    "Next.js",
+    "TypeScript",
+    "Portfolio",
+  ],
+  authors: [{ name: "Axel Soler" }],
+  creator: "Axel Soler",
+  publisher: "Axel Soler",
+  manifest: "/manifest.json",
+  icons: {
+    icon: "/favicon.ico",
+    apple: "/apple-icon.png",
+  },
+  openGraph: {
+    type: "website",
+    url: "https://axel-soler-dev.netlify.app/",
+    title: "Axel Soler | Full-Stack Developer",
+    description:
+      "Explore the projects and skills of Axel Soler, a passionate software developer.",
+    siteName: "Axel Soler Portfolio",
+    images: [
+      {
+        url: "https://media.licdn.com/dms/image/v2/C4D03AQFiBM2qEQ07GA/profile-displayphoto-shrink_200_200/profile-displayphoto-shrink_200_200/0/1654712370919?e=2147483647&v=beta&t=sYiRYzq33CxWZ-wNNHglFs4Z8BUFCJU5VCkS8buIzrk",
+      },
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    creator: "@AxelSoler18", // Reemplaza con tu usuario de Twitter
+    title: "Axel Soler | Software Developer",
+    description: "Portfolio of Axel Soler, a passionate software developer.",
+    images: ["https://media.licdn.com/dms/image/v2/C4D03AQFiBM2qEQ07GA/profile-displayphoto-shrink_200_200/profile-displayphoto-shrink_200_200/0/1654712370919?e=2147483647&v=beta&t=sYiRYzq33CxWZ-wNNHglFs4Z8BUFCJU5VCkS8buIzrk"],
+  },
 };
 
 export default function RootLayout({
-  children
+  children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" suppressHydrationWarning className="scroll-smooth overflow-x-auto">
+    <html
+      lang="en"
+      suppressHydrationWarning
+      className="scroll-smooth overflow-x-auto"
+    >
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased text-black dark:text-white bg-gradient-to-r from-slate-300 via-slate-100 to-slate-300 shadow-md dark:bg-gradient-to-r dark:from-[#0a0a0a] dark:via-[#004aad] dark:to-[#0a0a0a] transition-colors duration-500 ease-in-out`}
       >
