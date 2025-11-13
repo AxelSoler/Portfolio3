4839c2a127501301740ecc69abb0e96381e9ee5d
AxelSoler <axelss369@gmail.com>
2025-11-13 15:36:56 -0300

    Add more description

--- src/components/about/index.tsx
+++ src/components/about/index.tsx
@@ -14,12 +14,13 @@ const About = () => {
       id="about"
     >
       <ProfileCard />
-      <div className="flex flex-col items-center md:w-4/5 mt-4 md:mt-8 lg:mt-16">
-        <p className="text-center text-xl lg:text-4xl font-bold border-b border-black dark:border-white mb-4 md:mb-6 lg:mb-8">
+      <div className="flex flex-col items-center md:w-4/5 mt-4 md:mt-8">
+        <p className="text-center text-2xl lg:text-4xl font-bold border-b border-black dark:border-white mb-4">
           {t("haveIdea")}
           <br className="md:hidden" /> {t("makeItReal")}
         </p>
-        <div className="flex flex-col md:flex-row gap-4 md:gap-8 lg:gap-24 mb-4 md:mb-8 md:justify-center w-full font-bold">
+        <p className="md:text-lg lg:text-xl mb-6">{t("description")}</p>
+        <div className="flex flex-col md:flex-row gap-4 md:gap-8 lg:gap-24 mb-4 md:mb-8 lg:mb-12 md:justify-center w-full font-bold">
           <Link
             href="#professional"
             className="flex items-center justify-center w-3/4 mx-auto md:mx-0 md:w-[200px] md:text-xl gap-2 px-4 py-2 bg-gradient-to-r from-blue-500 to-indigo-600 text-white rounded-xl transition-all duration-300 shadow-[0_6px_25px_theme(colors.indigo.600/0.8)] hover:scale-105"
