6d6dd52576b7a372f2d562b5ed031651dbc71aff
AxelSoler <axelss369@gmail.com>
2025-11-14 15:08:01 -0300

    Move work experience into Experience and certi

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,7 +12,6 @@ export default function Home() {
       <Navbar />
       <About />
       <Projects />
-      <Professional />
       <Education />
       <ContactForm />
       <ContactButton />
--- src/components/education/index.tsx
+++ src/components/education/index.tsx
@@ -1,20 +1,23 @@
 import { useTranslations } from "next-intl";
-import Schooldescription from "./schoolDescription";
+import ExperienceDescription from "./experiencedescription";
 import microverse from "@/img/profile/Microverse_logo.png";
 import eest from "@/img/profile/eest_logo.jpg";
 import unmdp from "@/img/profile/unmdp_logo.jpg";
 
 const Education = () => {
-  const t = useTranslations("Education");
+  const t = useTranslations("Experience");
   return (
     <section className="w-full md:w-4/5" id="education">
       <div className="p-4 md:p-6 lg:p-16 bg-gray-600/30 dark:bg-black/60 shadow-[0_6px_25px_theme(colors.indigo.600/0.8)] dark:shadow-[0_6px_25px_rgba(59,130,246,0.25)] rounded-2xl">
         <h2 className="text-2xl md:text-3xl lg:text-4xl font-bold underline mb-4 text-center">
           {t("title")}
         </h2>
-        <Schooldescription section="microverse" image={microverse} />
-        <Schooldescription section="university" image={unmdp} />
-        <Schooldescription section="highschool" image={eest} />
+        <div className="grid lg:grid-cols-2 gap-8 mt-4 lg:mt-8">
+          <ExperienceDescription section="Goodsoftwaredev" image={microverse} />
+          <ExperienceDescription section="microverse" image={microverse} />
+          <ExperienceDescription section="university" image={unmdp} />
+          <ExperienceDescription section="highschool" image={eest} />
+        </div>
       </div>
     </section>
   );
