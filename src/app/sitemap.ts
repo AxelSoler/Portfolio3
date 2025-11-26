92c5f87e33e08d5c514b3be7c884de3103bec9f8
AxelSoler <axelss369@gmail.com>
2025-11-26 15:28:00 -0300

    Remove index id

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -8,11 +8,5 @@ export default function sitemap(): MetadataRoute.Sitemap {
       changeFrequency: 'daily',
       priority: 1.0,
     },
-    {
-      url: 'https://axelsoler.dev/#about',
-      lastModified: new Date(),
-      changeFrequency: 'monthly',
-      priority: 0.8,
-    },
   ]
 }
