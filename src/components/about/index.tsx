2d6a500a31cfab4fc9945faa1b33cb6f1d0642da
AxelSoler <axelss369@gmail.com>
2025-11-27 18:27:01 -0300

    Remove experience link

--- src/components/about/index.tsx
+++ src/components/about/index.tsx
@@ -21,22 +21,13 @@ const About = () => {
           <br className="md:hidden" /> {t("makeItReal")}
         </p>
         <p className="md:text-lg lg:text-xl mb-6">{t("description")}</p>
-        <div className="flex gap-4 md:gap-8 lg:gap-24 mb-4 md:mb-8 lg:mb-12 md:justify-center w-full font-bold">
-          <Link
-            href="#professional"
-            className="flex items-center justify-center w-full md:w-[200px] md:text-xl gap-2 px-4 py-2 rounded-xl backdrop-blur-md border-2 border-blue-500/40 hover:border-blue-400 hover:shadow-[0_0_15px_#6366f1] transition-all duration-300 hover:scale-105"
-          >
-            <LuBriefcaseBusiness className="text-xl" />
-            {t("experienceLink")}
-          </Link>
-          <Link
-            href="#contact"
-            className="flex items-center justify-center w-full md:w-[200px] md:text-xl gap-2 px-4 py-2 rounded-xl transition-all duration-300 hover:scale-105 border-2 border-violet-500 hover:shadow-[0_0_15px_#8b5cf6]"
-          >
-            <CiMail className="text-3xl" />
-            {t("contactLink")}
-          </Link>
-        </div>
+        <Link
+          href="#contact"
+          className="flex items-center justify-center mb-4 md:mb-8 lg:mb-12 w-full md:w-[200px] md:text-xl gap-2 px-4 py-2 rounded-xl backdrop-blur-md border-2 border-blue-500/40 hover:border-blue-400 hover:shadow-[0_0_15px_#6366f1] transition-all duration-300 hover:scale-105"
+        >
+          <CiMail className="text-3xl" />
+          {t("contactLink")}
+        </Link>
         <div className="grid grid-cols-1 lg:grid-cols-2 gap-4 md:gap-8 lg:gap-16 text-left text-sm md:text-lg">
           <AboutSection />
           <Skills />
